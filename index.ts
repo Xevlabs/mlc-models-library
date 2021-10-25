4bad8cb610fba554ea2e33a270daa5eb073660a8
AmineYoussef <amine.youssef@xevlabs.com>
2021-10-25 09:17:05 +0100

    export role enum

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export * from './auth/auth.model';
 export * from './common/common.model';
 export * from './user/user.model';
 export * from './translation/translation.model';
+export * from './roles/roles.enum';
