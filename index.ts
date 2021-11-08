5c91967fc49aea2ee65d4a9b00f93fb350d6d5a5
AmineYoussef <amine.youssef@xevlabs.com>
2021-11-08 10:21:43 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from './auth/auth.model';
 export * from './common/common.model';
 export * from './user/user.model';
+export * from './user/title.enum';
 export * from './translation/translation.model';
 export * from './roles/roles.enum';
 export * from './connection-log/connection-log.model';
