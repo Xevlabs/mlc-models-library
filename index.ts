0153e220b7d96ec21ae63ede9cb2c0af0f010b25
AmineYoussef <amine.youssef@xevlabs.com>
2021-11-09 15:48:47 +0100

    added user exports

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
 // MODEL EXPORTS
 export * from './auth/auth.model';
 export * from './common/common.model';
-export * from './user/user.model';
+export * from './user/index';
 export * from './translation/translation.model';
 export * from './connection-log/connection-log.model';
 export * from './settings/settings.model';
-export * from './user/client.model';
-export * from './user/client-info.model';
-export * from './user/client-kyc.model';
-export * from './user/fireblocks-user.model';
-export * from './user/treezor-user.model';
 export * from './document/document.model';
 
 // ENUM EXPORTS
