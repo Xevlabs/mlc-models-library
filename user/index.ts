b87485e56a60bd9e5a57800babbd43472207d114
RemyBosviel <62258673+RemyBosviel@users.noreply.github.com>
2021-11-17 15:09:48 +0100

    fixed ClientKycModel to UserKycModel to match strapi object (#31)

--- user/index.ts
+++ user/index.ts
@@ -1,5 +1,5 @@
 export * from './client-info.model';
-export * from './client-kyc.model';
+export * from './user-kyc.model';
 export * from './client.model';
 export * from './fireblocks-user.model';
 export * from './profile.model';
