5c24b1e96d14ab8bddc3390c80ca19baacdc80c3
Rémy Bosviel <remy.bosviel@gmail.com>
2021-12-01 15:26:58 +0100

    added extendedClientEditionFormModel to update username on email change

--- user/client.model.ts
+++ user/client.model.ts
@@ -21,6 +21,10 @@ export interface ClientRegistrationFormModel extends Omit<ClientEditionFormModel
     address : AddressFormModel
 }
 
+export interface ExtendedClientEditionFormModel extends ClientEditionFormModel {
+    username: string;
+}
+
 export interface ClientEditionFormModel {
     firstName: string,
     lastName: string,
