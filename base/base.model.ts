e11dda5d077313bbc69f502f8067ffe6c295fc97
AmineYoussef <amine.youssef@xevlabs.com>
2021-12-07 15:30:05 +0100

    note model adjustments

--- base/base.model.ts
+++ base/base.model.ts
@@ -1,4 +1,5 @@
 import { DbLinkModel, TimestampModel } from '../common/common.model';
+import { ClientModel } from '../user';
 
 export interface BaseModel extends TimestampModel {
     id: number,
@@ -9,4 +10,9 @@ export interface BaseModel extends TimestampModel {
 export interface StrapiBaseModel {
     updated_by_user: DbLinkModel,
     created_by_user: DbLinkModel
+}
+
+export interface PopulatedBaseModel {
+    updated_by_user: ClientModel,
+    created_by_user: ClientModel
 }
