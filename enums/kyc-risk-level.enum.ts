3ed53d20bc77e7a70e28b313b86adfc5f805b436
Rémy Bosviel <remy.bosviel@gmail.com>
2021-12-08 09:09:34 +0100

    added kyc base score

---
+++ enums/kyc-risk-level.enum.ts
@@ -0,0 +1,6 @@
+export enum KycRiskLevelEnum {
+    WEAK = 'WEAK',
+    MODERATE = 'MODERATE',
+    HIGH = 'HIGH',
+    UNACCEPTABLE = 'UNACCEPTABLE'
+}
--- user/kyc/client-kyc.model.ts
+++ user/kyc/client-kyc.model.ts
@@ -1,13 +1,28 @@
 import { BaseModel } from '../../base/base.model';
 import { KycStatusEnum } from '../../enums/kyc-status.enum';
 import { TreezorKycAbstractModel } from './treezor-kyc-abstract.model';
+import { KycRiskLevelEnum } from '../../enums/kyc-risk-level.enum';
 
-export interface ClientKycModel extends BaseModel, Omit<ClientKycFormModel, 'treezorKycAbstract'> {
+export interface ClientKycModel extends BaseModel, Omit<StrapiClientKycCreationModel, 'treezorKycAbstract'>, Partial<ClientKycScoreRelatedFields> {
     treezorKycAbstract: TreezorKycAbstractModel
 }
 
-export interface ClientKycFormModel {
+export interface StrapiClientKycCreationModel {
     status: KycStatusEnum,
     user: string,
     treezorKycAbstract?: number
+}
+
+export interface StrapiClientKycEditionModel extends ClientKycScoreRelatedFields {
+    status?: KycStatusEnum
+}
+
+export interface KycBaseScoreFormModel {
+    baseScore: number
+}
+
+interface ClientKycScoreRelatedFields {
+    baseScore: number,
+    currentScore: number,
+    riskProfile: KycRiskLevelEnum
 }
