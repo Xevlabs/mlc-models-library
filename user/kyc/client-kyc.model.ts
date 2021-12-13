252f0ffe7883fa7dede81cfca06f21d5868c12f8
AmineYoussef <amine.youssef@xevlabs.com>
2021-12-13 15:05:50 +0100

    Update client-kyc.model.ts

--- user/kyc/client-kyc.model.ts
+++ user/kyc/client-kyc.model.ts
@@ -2,7 +2,7 @@ import { BaseModel } from '../../base/base.model';
 import { KycStatusEnum } from '../../enums/kyc-status.enum';
 import { TreezorKycAbstractModel } from './treezor-kyc-abstract.model';
 import { KycRiskLevelEnum } from '../../enums/kyc-risk-level.enum';
-import { DbLinkModel } from '@/typings';
+import { DbLinkModel } from '../../common/common.model';
 
 export interface ClientKycModel extends BaseModel, Omit<StrapiClientKycCreationModel, 'treezorKycAbstract'>, Partial<ClientKycScoreRelatedFields> {
     treezorKycAbstract: TreezorKycAbstractModel
