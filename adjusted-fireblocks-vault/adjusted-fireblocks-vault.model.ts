24d08fbbd29daad4ccc59607865f22c96b9ee6ff
Rémy Bosviel <remy.bosviel@gmail.com>
2022-01-31 11:13:30 +0100

    changed adjusted fireblocks wallets to have values before and after transactions

--- adjusted-fireblocks-vault/adjusted-fireblocks-vault.model.ts
+++ adjusted-fireblocks-vault/adjusted-fireblocks-vault.model.ts
@@ -1,10 +1,9 @@
-import { SimplifiedTransactionWithFireblocksModel } from '../transaction/transaction.model';
 import { AdjustedExtendedFireblocksVaultAccount, ExtendedFireblocksVaultAccount } from '../common-fireblocks-vault/extended-fireblocks-vault-account';
 
 export interface AdjustedFireblocksVaultModel {
     adjustedVault: AdjustedExtendedFireblocksVaultAccount,
     isDifferentFromFireblocks: boolean,
     hasErrors: boolean,
-    movements: SimplifiedTransactionWithFireblocksModel[];
+    linkedTransactionsIds: number[];
     vault: ExtendedFireblocksVaultAccount
 }
