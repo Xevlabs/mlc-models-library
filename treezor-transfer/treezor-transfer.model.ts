b6be8511723d31f7f49f1cb6a1636949adacf174
RemyBosviel <62258673+RemyBosviel@users.noreply.github.com>
2022-02-17 17:55:42 +0100

    added the user to TreezorTransfers (#80)
    * added the user to TreezorTransfers
    
    * fixed possibly missing user

--- treezor-transfer/treezor-transfer.model.ts
+++ treezor-transfer/treezor-transfer.model.ts
@@ -2,15 +2,20 @@ import { BaseModel } from '../base/base.model';
 import { TreezorApiTransferModel } from '../treezor-api/treezor-api-transfer.model';
 import { TreezorApiTransferStatusEnum } from '../enums/treezor-api-transfer-status.enum';
 import { NonPopulatedTransactionModel } from '../transaction/transaction.model';
+import { ClientModel } from '../user/client.model';
+import { DbLinkModel } from '../common/common.model';
+import { TreezorTransferLogModel } from '../treezor-transfer-log/treezor-transfer-log.model';
 
-export interface TreezorTransferModel extends BaseModel, Omit<StrapiTreezorTransferFormModel, 'transaction'> {
+export interface TreezorTransferModel extends BaseModel, Omit<StrapiTreezorTransferFormModel, 'transaction' | 'user'> {
     transaction: NonPopulatedTransactionModel,
-    treezorTransferLogs: number[]
+    treezorTransferLogs: TreezorTransferLogModel[],
+    user?: ClientModel
 }
 
 export interface StrapiTreezorTransferFormModel extends Partial<Omit<TreezorApiTransferModel, 'amount' | 'transferStatus'>> {
     amount?: number,
     transaction?: number,
     transferStatus: TreezorApiTransferStatusEnum,
+    user?: DbLinkModel,
     treezorErrorResponse?: string
 }
