c37f23575118b82c1c9ad749baa73c15749dd0b5
Nurarara <nourelhouda.boukari@xevlabs.com>
2022-02-24 13:43:54 +0100

    added treezor-transfer-direction.enum

--- index.ts
+++ index.ts
@@ -59,6 +59,7 @@ export * from './enums/mlc-earning-type.enum';
 export * from './enums/transaction-error-code.enum';
 export * from './enums/transaction-error-level.enum';
 export * from './enums/inputType.enum'
+export * from './enums/treezor-transfer-direction.enum'
 
 // CONSTANTS EXPORTS
 export * from './constants/admin-roles.constant';
