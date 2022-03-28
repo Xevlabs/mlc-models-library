3ce67d0082047322e09491612fb53ed894acca88
Rémy Bosviel <remy.bosviel@gmail.com>
2022-03-28 16:22:39 +0200

    required changes

--- beneficiary/beneficiary.model.ts
+++ beneficiary/beneficiary.model.ts
@@ -1,17 +1,21 @@
 import { TimestampModel, TreezorUserModel } from '..';
 
-export interface BeneficiaryModel extends Omit<StrapiBeneficiaryFormModel, 'creditor'>, TimestampModel {
-    id: number,
+export interface BeneficiaryModel extends Omit<StrapiBeneficiaryFormModel, 'creditor'>, TimestampModel, ClientBeneficiaryModel {
     creditor: TreezorUserModel
 }
 
-export interface StrapiBeneficiaryFormModel {
-    ibanFullname: string,
-    DbtrIBAN: string,
+export interface StrapiBeneficiaryFormModel extends StrapiCommonBeneficiaryFields {
     ibanBic: string,
     creditor: number,
     treezorBeneficiaryId?: string,
     usableForSct: boolean
 }
 
-export interface ClientBeneficiaryModel extends Pick<BeneficiaryModel, 'id' | 'ibanFullname' | 'DbtrIBAN'> {}
+export interface ClientBeneficiaryModel extends StrapiCommonBeneficiaryFields {
+    id: number
+}
+
+interface StrapiCommonBeneficiaryFields {
+    ibanFullname: string,
+    DbtrIBAN: string
+}
