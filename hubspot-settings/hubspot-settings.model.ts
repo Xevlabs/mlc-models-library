4bc29a55077ef56ba51536eababbe59d367fb51f
Rémy Bosviel <remy.bosviel@gmail.com>
2022-06-14 15:37:02 +0200

    added ids to Hubspot settings. Added models for the hubspot service

--- hubspot-settings/hubspot-settings.model.ts
+++ hubspot-settings/hubspot-settings.model.ts
@@ -1,4 +1,8 @@
-export interface HubspotSettingsModel {
+export interface HubspotSettingsModel extends HubspotLabelSettingsModel, HubspotIdSettingsModel {}
+
+export interface TemplateHubspotSettingsModel extends HubspotLabelSettingsModel, Partial<HubspotIdSettingsModel> {}
+
+interface HubspotLabelSettingsModel {
     registrationPipelineLabel: string,
     registrationPersonalInfoStageLabel: string,
     registrationUserCreatedStageLabel: string,
@@ -6,4 +10,27 @@ export interface HubspotSettingsModel {
     registrationWaitingTreezorReviewStageLabel: string,
     registrationWaitingBaseScoreStageLabel: string,
     registrationValidationFinishedStageLabel: string
+}
+
+export interface HubspotIdSettingsModel {
+    registrationPipelineId: string,
+    registrationPersonalInfoStageId: string,
+    registrationUserCreatedStageId: string,
+    registrationWaitingValidPayinStageId: string,
+    registrationWaitingTreezorReviewStageId: string,
+    registrationWaitingBaseScoreStageId: string,
+    registrationValidationFinishedStageId: string
+}
+
+export interface HubspotPipelineTemplate {
+    id: string,
+    label: string,
+    displayOrder: number,
+    stages: HubspotStageTemplate[]
+}
+
+export interface HubspotStageTemplate {
+    id: string,
+    label: string,
+    displayOrder: number
 }
